9594700642d5c0432006f9f52c750e30d4dd3452
Thomas Weibenfalk <thomas.weibenfalk@gmail.com>
2019-06-08 11:05:54 +0200

    Fixed social links

--- src/components/Social.js
+++ src/components/Social.js
@@ -11,16 +11,16 @@ import { StyledSocial, StyledIcons } from './styles/StyledSocial';
 const Social = () => (
   <StyledSocial>
     <StyledIcons>
-      <a href="">
+      <a href="https://www.facebook.com/CinemaNord/">
         <img src={Facebook} alt="cn-facebook" />
       </a>
-      <a href="">
+      <a href="https://twitter.com/Lindemark">
         <img src={Twitter} alt="cn-twitter" />
       </a>
-      <a href="">
+      <a href="https://www.linkedin.com/in/andreas-lindemark/">
         <img src={LinkedIn} alt="cn-linkedin" />
       </a>
-      <a href="">
+      <a href="https://www.instagram.com/andreas_lindemark_cinemanord/?hl=sv">
         <img src={Instagram} alt="cn-instagram" />
       </a>
     </StyledIcons>
