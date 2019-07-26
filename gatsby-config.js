3b904eced6f0e9999c8af459bb7a61bbc8b850fb
Thomas Weibenfalk <thomas.weibenfalk@gmail.com>
2019-07-26 15:25:11 +0200

    Added google ad

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,6 +68,32 @@ module.exports = {
         trackingId: 'UA-57605980-1',
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'UA-57605980-1', // Google Analytics / GA
+          'AW-957797553', // Google Ads / Adwords / AW
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        // gtagConfig: {
+        //   optimize_id: 'OPT_CONTAINER_ID',
+        //   anonymize_ip: true,
+        //   cookie_expires: 0,
+        // },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          // exclude: ['/preview/**', '/do-not-track/me/too/'],
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
